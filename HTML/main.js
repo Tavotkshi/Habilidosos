8aab474ab289ce9c19c088541066ca69c91bcdff
Gustavo <gustavotakashi5@gmail.com>
2023-09-10 14:27:23 -0500

    página

---
+++ HTML/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', ()  =>{
+
+        document.querySelector('.nav-menu1').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-card', {delay: 500});
+ScrollReveal().reveal('.card-banner', {delay: 500});
+ScrollReveal().reveal('.card-banner1', {delay: 500});
